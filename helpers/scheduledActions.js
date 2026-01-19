c895e5764acc6fe4589e9ebed522b65f4eaa1c64
AndrewDyomin <dyomin.andrew1@gmail.com>
2026-01-19 09:35:45 +0200

    Update scheduledActions.js

--- helpers/scheduledActions.js
+++ helpers/scheduledActions.js
@@ -910,7 +910,7 @@ cron.schedule(
     try {
       console.log("Копирую заказы...");
       await getAll();
-      await axios.get(process.env.HELPER_URL, {timeout: 25000});
+      await axios.get(process.env.HELPER_URL);
       console.log("Called the assistant");
     } catch (err) {
       console.log("Called the assistant error: ", err);
