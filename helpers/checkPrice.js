d86164923d2a2c29b49cd57407bc36a723080599
AndrewDyomin <dyomin.andrew1@gmail.com>
2026-02-02 17:36:40 +0200

    Update checkPrice.js

--- helpers/checkPrice.js
+++ helpers/checkPrice.js
@@ -1,6 +1,7 @@
 const Product = require("../models/item");
-const axios = require("axios");
+// const axios = require("axios");
 const mongoose = require("mongoose");
+const fetch = require('node-fetch');
 // const MoteaItem = require("../models/moteaItem");
 const cheerio = require('cheerio');
 
@@ -57,61 +58,162 @@ function sleep(ms) {
 //   return arrayCopy;
 // };
 
-function extractGa4Data(html) {
-  const startToken = '<script type="text/x-magento-init">';
-  const endToken = "</script>";
+// function extractGa4Data(html) {
+//   const startToken = '<script type="text/x-magento-init">';
+//   const endToken = "</script>";
 
-  const startIndex = html.indexOf(startToken);
-  const endIndex = html.indexOf(endToken, startIndex);
+//   const startIndex = html.indexOf(startToken);
+//   const endIndex = html.indexOf(endToken, startIndex);
 
-  if (startIndex === -1 || endIndex === -1) {
-    console.log("Скрипт не найден");
-    return null;
+//   if (startIndex === -1 || endIndex === -1) {
+//     console.log("Скрипт не найден");
+//     return null;
+//   }
+
+//   const jsonScript = html
+//     .slice(startIndex + startToken.length, endIndex)
+//     .trim();
+
+//   try {
+//     const parsed = JSON.parse(jsonScript);
+//     const data = parsed["*"]?.magepalGtmDatalayer?.data;
+//     return data || null;
+//   } catch (err) {
+//     console.error("Ошибка при разборе JSON:", err.message);
+//     return null;
+//   }
+// }
+
+async function extractSearchData(body) {
+  const MAX_SIZE = 500_000; // 500 KB
+  let size = 0;
+  const chunks = [];
+
+  for await (const chunk of body) {
+    size += chunk.length;
+    if (size > MAX_SIZE) break;
+    chunks.push(chunk);
   }
 
-  const jsonScript = html
-    .slice(startIndex + startToken.length, endIndex)
-    .trim();
+  const html = Buffer.concat(chunks).toString('utf8');
+  const $ = cheerio.load(html);
 
-  try {
-    const parsed = JSON.parse(jsonScript);
-    const data = parsed["*"]?.magepalGtmDatalayer?.data;
-    return data || null;
-  } catch (err) {
-    console.error("Ошибка при разборе JSON:", err.message);
-    return null;
-  }
-}
-
-function extractSearchData(html) {
-  const startToken = '<ol class="products list items product-items motea-list">';
-  const endToken = "</ol>";
-
-  const startIndex = html.indexOf(startToken);
-  const endIndex = html.indexOf(endToken, startIndex);
-
-  if (startIndex === -1 || endIndex === -1) {
-    console.log("Список не найден");
-    return null;
-  }
-
-  const listHtml = html.slice(startIndex, endIndex + endToken.length);
-
-  const $ = cheerio.load(listHtml);
+  const container = $('ol.products.list.items.product-items.motea-list').first();
 
   const result = [];
 
-  $('li[data-producturl]').each((_, li) => {
+  container.find('li[data-producturl]').each((_, li) => {
     const url = $(li).attr('data-producturl');
 
-    const price = $(li).find('span.special-price > span.price-container > span.price-wrapper[data-price-amount]').attr('data-price-amount') || null;
+    // собираем ВСЕ цены
+    const prices = $(li)
+      .find('span.price-wrapper[data-price-amount]')
+      .map((_, el) => Number($(el).attr('data-price-amount')))
+      .get()
+      .sort((a, b) => a - b);
 
-    if (url && price) {
-      result.push({ url, price });
+    if (!url || prices.length === 0) return;
+
+    let priceData;
+
+    if (prices.length === 1) {
+      priceData = {
+        price: prices[0],
+      };
+    } else {
+      priceData = {
+        minPrice: prices[0],
+        maxPrice: prices[prices.length - 1],
+        prices,
+      };
     }
+
+    result.push({
+      url,
+      ...priceData,
+    });
   });
 
-  return result.length > 0 ? result : null;
+  return result.length ? result : null;
+}
+
+// function extractSearchData(html) {
+//   const startToken = '<ol class="products list items product-items motea-list">';
+//   const endToken = "</ol>";
+
+//   const startIndex = html.indexOf(startToken);
+//   const endIndex = html.indexOf(endToken, startIndex);
+
+//   if (startIndex === -1 || endIndex === -1) {
+//     console.log("Список не найден");
+//     return null;
+//   }
+
+//   const listHtml = html.slice(startIndex, endIndex + endToken.length);
+
+//   const $ = cheerio.load(listHtml);
+
+//   const result = [];
+
+//   $('li[data-producturl]').each((_, li) => {
+//     const url = $(li).attr('data-producturl');
+
+//     const price = $(li).find('span.special-price > span.price-container > span.price-wrapper[data-price-amount]').attr('data-price-amount') || null;
+
+//     if (url && price) {
+//       result.push({ url, price });
+//     }
+//   });
+
+//   return result.length > 0 ? result : null;
+// }
+
+async function extractHtml(body) {
+  let html = '';
+
+  for await (const chunk of body) {
+    html += chunk.toString();
+  }
+
+  const $ = cheerio.load(html);
+  const container = $('.product-info-price').first();
+  if (!container.length) return null;
+
+  const prices = [];
+
+  container.find('.price').each((_, el) => {
+    const text = $(el).text().trim();
+    if (text) prices.push(text);
+  });
+
+  if (!prices.length) return null;
+
+  prices.sort((a, b) => a - b);
+
+  const parse = (t) =>
+    Number(t.replace('€', '').replace(',', '.').trim());
+
+  let upper = null;
+  let price = null;
+  let lower = null;
+
+  if (prices.length === 1) {
+    price = prices[0];
+  } else if (prices.length === 2) {
+    price = prices[0];
+    lower = prices[1];
+  } else {
+    const last = prices.slice(-3);
+    [upper, price, lower] = last;
+  }
+
+  return {
+    upper: upper ? parse(upper) : null,
+    price: price ? parse(price) : null,
+    lower: lower ? parse(lower) : null,
+    currency: 'EUR',
+    raw: prices
+  };
 }
 
 //    OLD VERSION
@@ -224,8 +326,10 @@ async function checkPrice() {
     await mongoose.connect(MAIN_DB_URI);
     console.log("Connected to main DB");
     const now = new Date();
+    const yesterday = new Date(now);
+    yesterday.setDate(now.getDate() - 1);
     const exchangeRate = 49.5;
-    const dbItems = await Product.find({}, { article: 1, price: 1, moteaPrice: 1, linkInMotea: 1, _id: 0 });
+    const dbItems = await Product.find({}, { article: 1, price: 1, moteaPrice: 1, linkInMotea: 1, _id: 1 });
     let c = 0;
 
     for (let i = 0; i < dbItems.length; i++) {
@@ -241,7 +345,7 @@ async function checkPrice() {
       }
 
       const dateDifference = now - new Date(item.moteaPrice.date);
-      if (dateDifference < 86400000 || blackList.includes(item.article)) {
+      if (dateDifference < 86400000) {
         continue;
       }
 
@@ -251,51 +355,39 @@ async function checkPrice() {
       }
 
       try {
-        const targetArray = await Product.find({article: item.article}).exec()
-        for (const target of targetArray) {
-          await Product.findByIdAndUpdate(target._id, {moteaPrice: { UAH: target?.moteaPrice?.UAH, date: now }})
-        }
-        let response = await axios.get(link);
+        await Product.findByIdAndUpdate(item._id, {moteaPrice: { UAH: item?.moteaPrice?.UAH, date: now }})
+        let response = await fetch(link);
         await sleep(1000);
-        let data = extractGa4Data(response.data);
-        if (!data) {
-          console.log('GA4 data not found');
-          for (const target of targetArray) {
-            await Product.findByIdAndUpdate(target._id, {moteaPrice: { UAH: null, date: now }})
-          }
-          continue;
-        }
+        let data = await extractHtml(response.body);
         response = null;
-        const mItem = data.find((i) => i?.product?.price)?.product;
-        let mPrice = Math.round(mItem?.price * exchangeRate) || null;
+        
+        let mPrice = data?.price || null;
 
-        if (!mItem) {
-          const search = await axios.get(`https://www.motea.com/en/catalogsearch/result/?q=${item.article}`);
-          data = extractSearchData(search.data)
+        if (!mPrice) {
+          console.log('search starting')
+          const search = await fetch(`https://www.motea.com/en/catalogsearch/result/?q=${item.article}`);
+
+          data = extractSearchData(search.body)
           const t = data.find(i => i.url.includes(item.article.toLowerCase()));
           if (!t) {
-            console.log('article not found')
-            for (const target of targetArray) {
-              await Product.findByIdAndUpdate(target._id, {moteaPrice: { UAH: null, date: now }})
-            }
+            console.log(item.article, 'article not found')
+            await Product.findByIdAndUpdate(item._id, {moteaPrice: { UAH: null, date: now }})
             continue;
           }
           mPrice = Math.round(Number(t.price) * exchangeRate) || null
         }
 
         if (!mPrice) {
-          for (const target of targetArray) {
-            await Product.findByIdAndUpdate(target._id, {moteaPrice: { UAH: null, date: now }})
-          }
+          await Product.findByIdAndUpdate(item._id, {moteaPrice: { UAH: null, date: now }})
           continue;
         }
 
-        for (const target of targetArray) {
-          await Product.findByIdAndUpdate(target._id, {moteaPrice: { UAH: mPrice, date: now }})
-        }
+        await Product.findByIdAndUpdate(item._id, {moteaPrice: { UAH: mPrice, date: now }})
+
       } catch (err) {
         if (err?.response?.status === 429) {
           console.log("Block 429!!!");
+          await Product.findByIdAndUpdate(item._id, {moteaPrice: { UAH: item?.moteaPrice?.UAH, date: yesterday }})
           await sleep(20000);
         } else if (err.message === 'Client must be connected before running operations') {
           await sleep(300000);
