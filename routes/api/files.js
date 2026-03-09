08908bde44a4225f0696ade8a80e4f8e3801eafa
AndrewDyomin <dyomin.andrew1@gmail.com>
2026-03-09 17:25:27 +0200

    Update files.js

--- routes/api/files.js
+++ routes/api/files.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const filesController = require("../../controllers/files");
 const { upload } = require("../../middlewares/upload");
+const isAuth = require("../../middlewares/isAuth");
 
 const router = express.Router();
 
-router.post("/invoiceParser", upload.single("invoice"), filesController.uploadInvoice);
-router.post("/download-table-for-broker", filesController.downloadBrokerTable);
+router.post("/invoiceParser", isAuth, upload.single("invoice"), filesController.uploadInvoice);
+router.post("/download-table-for-broker", isAuth, filesController.downloadBrokerTable);
+router.get("/zakupka.xml", (req, res) => { const filePath = path.join(__dirname, "../../", "public", "xml", "zakupka.xml"); res.sendFile(filePath);})
 
 module.exports = router;
